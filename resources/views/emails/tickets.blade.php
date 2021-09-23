79bf015f276be86dc447079baf07b33d92aeea90
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-09-23 08:36:54 +0200

    add messaggio email

--- resources/views/emails/tickets.blade.php
+++ resources/views/emails/tickets.blade.php
@@ -1,1 +1,9 @@
-<h2>ciao hai ricevuto un 'email</h2>
+<div >
+    <h1>Salve, Admin</h1>
+    <p>Hai ricevuto una mail da</p>
+    <p>Nome : {{ $lead->name }}</p>
+    <p>Email : <a href="#">{{ $lead->email }}</a></p>
+    <span>il {{$lead->created_at}}</span>
+    <h5 class="my-2">Messaggio</h5>
+    <p>{{ $lead->message }}</p>
+</div>
